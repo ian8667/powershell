bc87147b1f1b3d8d64b62b2ce4bca6f0e5ddd915
Ian <ian3924@yahoo.co.uk>
2017-11-12 17:48:13 +0000

    Powershell configuration file
    Microsoft PowerShell profile file located in the directory determined by the variable '$profile'.

---
+++ Microsoft.PowerShellISE_profile.ps1
@@ -0,0 +1,111 @@
+#------------------------------------------------
+# My local Powershell configuration file.
+# Microsoft.PowerShellISE_profile.ps1
+#
+# Last updated: 12 November 2017
+#------------------------------------------------
+
+# Source: http://dougfinke.com/blog/index.php/2009/02/03/powershell-ise-cream/
+Function Expand-Alias {
+
+  $content=$psise.CurrentOpenedFile.Editor.text
+
+  [System.Management.Automation.PsParser]::Tokenize($content, [ref] $null) |
+
+    Where { $_.Type -eq 'Command'} |
+
+    Sort StartLine, StartColumn -Desc | Do-Expansion
+
+}
+
+
+Function Expand-CurrentAlias {
+
+  $CaretColumn = $psise.CurrentOpenedFile.Editor.CaretColumn
+  $content=$psise.CurrentOpenedFile.Editor.text
+  [System.Management.Automation.PsParser]::Tokenize($content, [ref] $null) |
+
+    Where { $_.Type -eq 'Command'} |
+       Where { $CaretColumn -eq $_.StartColumn -OR
+            $CaretColumn -eq $_.EndColumn} | Do-Expansion
+
+}
+
+Filter Do-Expansion {
+
+    if ($_.Content -eq '?') {
+        $result = Get-Command '`?' -CommandType Alias
+    } else {
+        $result = Get-Command $_.Content -CommandType Alias -ErrorAction SilentlyContinue
+    }
+
+    if($result) {
+
+        $psise.CurrentOpenedFile.Editor.Select($_.StartLine,$_.StartColumn,$_.EndLine,$_.EndColumn)
+        $psise.CurrentOpenedFile.Editor.InsertText($result.Definition)
+
+    }
+
+} #end function Do-Expansion
+
+
+#$null = $psISE.CustomMenu.Submenus.Add("Expand Alias", {Expand-Alias}, 'Ctrl+Shift+E')
+#$null = $psISE.CustomMenu.Submenus.Add("Expand Current Alias", {Expand-CurrentAlias}, 'Ctrl+Shift+W')
+
+#----------------------------------------------
+# Ian added items.
+#----------------------------------------------
+
+#------------------------------------------------
+# Create some aliases.
+#------------------------------------------------
+Set-Alias view c:\windows\notepad.exe
+Set-Alias vi   c:\windows\notepad.exe
+
+#------------------------------------------------
+# Go to our preferred startup location.
+#------------------------------------------------
+Set-Location C:\Family\powershell
+Clear-Host
+
+#------------------------------------------------
+# Welcome message and initial setup.
+#------------------------------------------------
+Write-Host "You are now entering PowerShell : " $env:Username
+Get-Date
+Write-Host "We're currently in directory $(Get-Location)"
+
+#------------------------------------------------
+# Create some functions.
+#------------------------------------------------
+function prompt
+{
+    #$width = ($Host.UI.RawUI.WindowSize.Width - 2 - $(Get-Location).ToString().Length)
+    $width = 60;
+    $hr = New-Object System.String @('-', $width)
+
+    $currtime=$(get-date).Tostring("HH:mm:ss")
+    Write-Host -ForegroundColor Red $(Get-Location) $hr
+
+    #function prompt {"Mugunth: $(get-date)>"}
+    #Write-Host ("PS " + $($currtime) +"==>")
+    "PS " + $($currtime) + "==> "
+    #"Mugunth: $(get-date)> "
+    return " "
+}
+
+#------------------------------------------------
+# Misc items.
+#------------------------------------------------
+Set-PSDebug -Strict
+
+# Once you've written a number of PowerShell scripts, you
+# might find it useful to collect them in one place and
+# create a PSDrive named scripts: to find them quickly.
+# You could add the following to your profile to create
+# such a PSDrive.
+#New-PSdrive -name scripts -PSprovider filesystem -root C:\Family\Ian
+
+#----------------------------------------------
+# End of Ian added items.
+#----------------------------------------------
