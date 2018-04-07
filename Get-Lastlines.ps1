0c53df9fa0f01a2112a0a6bf00386c184176f606
Ian <ian3924@yahoo.co.uk>
2018-04-07 19:42:10 +0100

    Final version of the code
    Considered to be the final version of the code following it's development.

--- Get-Lastlines.ps1
+++ Get-Lastlines.ps1
@@ -1,5 +1,47 @@
-﻿# Read the last N bytes of a file.
-#
+﻿<#
+.SYNOPSIS
+
+Lists the last N bytes of a file.
+
+.DESCRIPTION
+
+Uses the FileStream method 'Seek' in order to move the current position
+of the stream to a given value towards the end of the stream in order
+to read (list) the last N bytes of a file.
+
+The user defined variables 'path', 'buffersize' and 'seekPos' can be
+modified in function Get-Parameters as appropriate. These variables
+should be modified by the user prior to running the script.
+
+.EXAMPLE
+
+./Get-Lastlines.ps1
+
+No parameters are required
+
+.INPUTS
+
+None, no .NET Framework types of objects are used as input.
+
+.OUTPUTS
+
+No .NET Framework types of objects are output from this script.
+
+.NOTES
+
+File Name    : Get-Lastlines.ps1
+Author       : Ian Molloy
+Last updated : 2018-04-06
+
+.LINK
+
+Online notepad
+http://www.rapidtables.com/tools/notepad.htm
+
+Microsoft.PowerShell.Core help topic 'about'
+https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/?view=powershell-6
+#>
+
 [CmdletBinding()]
 Param () #end param
 
@@ -16,12 +58,12 @@ function Get-Parameters {
         path              = 'C:\test\gashInput.txt';
 
         # Int32 Struct. This variable serves a dual purpose as:
-        #  1. The buffer size used by the stream.
-        #  2. Data buffer size when reading a block of bytes by
-        #     the filestream 'Read' method.
+        #   1. The buffer size used by the stream.
+        #   2. Data buffer size when reading a block of bytes by
+        #      the filestream 'Read' method.
         buffersize        = 4096;
 
-        # Int32 Struct. Sets the current position of this stream to
+        # Int64 Struct. Sets the current position of this stream to
         # the given value. ie, the total number of bytes from the end
         # of the file. Adjust this figure accordingly depending upon
         # the amount of data from the end of the file you wish to look
@@ -29,10 +71,10 @@ function Get-Parameters {
         #
         # So if you're interested in the last 15Kb of the file, for
         # example, set this variable as:
-        # seekPos = 15KB;
-        seekPos           = 12KB;
-    }
+        # seekPos           = [System.Convert]::ToInt64(15Kb);
+        seekPos           = [System.Convert]::ToInt64(2kb);
 
+    }
 
   }
 
@@ -92,6 +134,7 @@ function Get-InputFilestream {
 ##=============================================
 
 $param = Get-Parameters;
+Set-Variable -Name "param" -Option ReadOnly -Description "Contains program parameters";
 
 # Specifies the end of the stream as a reference point to seek from.
 # We do this because we're interested in the end of the file not the
@@ -99,10 +142,12 @@ $param = Get-Parameters;
 $theEnd = [System.IO.SeekOrigin]::End;
 
 # Total number of bytes read into the data buffer or zero if the end
-# of the stream is reached.
+# of the stream (EOF) is reached.
 $bytesRead = 0;
 
 $utf8 = New-Object -TypeName System.Text.UTF8Encoding -ArgumentList $false, $true;
+
+New-Variable -Name "EOF" -Value 0 -Description "End of file marker" -Option Constant;
 
 $param.buffersize = [System.Math]::Abs($param.buffersize);
 $dataBuffer = New-Object byte[] $param.buffersize;
@@ -111,36 +156,28 @@ try {
   $fis = Get-InputFilestream -Filename $param.path -Buffsize $param.buffersize;
 
   # Find out which is the smallest value. This ensures we don't
-  # attempt to move the file pointer before the beginning of the
-  # file if the data buffer is larger than the file. Failure to
-  # do this results in the error:
-  # "An attempt was made to move the file pointer before the beginning of the file."
+  # attempt to move the file position (pointer) before the
+  # beginning of the file if the data buffer is larger than the
+  # file. Failure to do so results in the error:
+  #   "An attempt was made to move the file pointer before the beginning of the file."
   $lookBytes = [System.Math]::Min($fis.Length, $param.seekPos);
 
 
   # In order to seek to a new position backwards from the end of the
   # stream, the first parameter of method 'Seek' HAS to be a
-  # negative number. This allows us to read the last few N bytes
-  # when we read from the stream.
+  # negative number. This moves the current position towards the end
+  # of the stream and thus allows us to read the last few N bytes
+  # when we start reading from that position to the end the stream.
   $lookBytes = [System.Math]::Abs($lookBytes) * -1;
   $fis.Seek($lookBytes, $theEnd) | Out-Null;
 
-  #$lookBytes = [System.Math]::Abs($lookBytes)
-  $bytesRead = $fis.Read($dataBuffer, 0, $param.buffersize);
-  #Write-Host ('Number of bytes read into the buffer: {0}' -f $bytesRead);
-  while ($bytesRead -ne 0) {
+  $bytesRead = $fis.Read($dataBuffer, 0, $dataBuffer.Length);
+  while ($bytesRead -ne $EOF) {
     $str = $utf8.GetString($dataBuffer);
     Write-Output $str;
 
-    $bytesRead = $fis.Read($dataBuffer, 0, $param.buffersize);
+    $bytesRead = $fis.Read($dataBuffer, 0, $dataBuffer.Length);
   }
-
-
-  # Write some blank lines
-  #foreach ($num in 1..3) {Write-Host ''}
-
-  #$str = $utf8.GetString($dataBuffer);
-  #Write-Host $str;
 
 } catch {
   Write-Error -Message $Error[0];
@@ -149,7 +186,7 @@ try {
   $fis.Close();
 }
 
-Write-Host 'End of test';
+Write-Host 'End of run';
 ##=============================================
 ## END OF SCRIPT: Get-Lastlines.ps1
 ##=============================================
