683f3be8a07fd0e3172afd3ccfba95ab4a9f4252
ian8667 <ianm785@outlook.com>
2018-08-11 21:29:11 +0100

    Added file 'ZipMyDirectory.ps1' to the repository.

---
+++ ZipMyDirectory.ps1
@@ -0,0 +1,23 @@
+﻿[CmdletBinding()]
+Param (
+) #end param
+
+$sourceDir = "C:\Family\BAE";
+$destinationFile = "C:\Test\gashfile.zip";
+
+If (Test-Path -Path $destinationFile) {Remove-Item -Path $destinationFile}
+
+Add-Type -AssemblyName "System.IO.Compression.FileSystem";
+
+$opt = [System.IO.Compression.CompressionLevel]::Optimal;
+
+Write-Output ('Zipping directory {0}' -f $sourceDir);
+[System.IO.Compression.ZipFile]::CreateFromDirectory( `
+            $sourceDir, `
+            $destinationFile, `
+            $opt, `
+            $false);
+Get-ChildItem $destinationFile;
+
+Write-Output '';
+Write-Output 'All done now';
