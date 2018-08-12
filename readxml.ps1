6a661af17e01a250940f871fab92f949003766d1
ian8667 <ianm785@outlook.com>
2018-08-12 19:39:20 +0100

    Added function Get-XmlReader.

--- readxml.ps1
+++ readxml.ps1
@@ -2,6 +2,41 @@
 [CmdletBinding()]
 Param () #end param
 
+#region ***** Function Get-XmlReader *****
+function Get-XmlReader {
+[CmdletBinding()]
+[OutputType([System.Xml.XmlReader])]
+Param (
+        [parameter(Mandatory=$true,
+                   HelpMessage="XML file to read")]
+        [ValidateNotNullOrEmpty()]
+        [String]$Filename
+      ) #end param
+
+BEGIN {
+$xsettings = New-Object -TypeName 'System.Xml.XmlReaderSettings';
+$xsettings.Async = $false;
+$xsettings.CheckCharacters = $true;
+$xsettings.CloseInput = $true;
+$xsettings.ConformanceLevel = [System.Xml.ConformanceLevel]::Document;
+$xsettings.DtdProcessing = [System.Xml.DtdProcessing]::Ignore;
+$xsettings.IgnoreComments = $true;
+$xsettings.IgnoreProcessingInstructions = $true;
+$xsettings.IgnoreWhitespace = $true;
+
+$xobj = [System.Xml.XmlReader]::Create($Filename, $xsettings);
+
+}
+
+PROCESS {}
+
+END {
+  return $xobj;
+}
+
+}
+#endregion ***** End of function Get-XmlReader *****
+
 ##=============================================
 ## SCRIPT BODY
 ## Main routine starts here
@@ -10,76 +45,66 @@ Set-StrictMode -Version Latest;
 $ErrorActionPreference = "Stop";
 
 $xmlline = New-Object -TypeName 'System.Text.StringBuilder' -ArgumentList 100;
-$settings = New-Object -TypeName System.Xml.XmlReaderSettings;
-$settings.Async = $true;
-$settings.CheckCharacters = $true;
-$settings.CloseInput = $true;
-$settings.ConformanceLevel = [System.Xml.ConformanceLevel]::Document;
-$settings.IgnoreWhitespace = $true;
 $ff = 'C:\Family\ian\hello.xml';
+$xreader = Get-XmlReader $ff;
+# An start element tag, ie <item>.
 $element = [System.Xml.XmlNodeType]::Element;
+# An end element tag ie ie <item>.
 $endelement = [System.Xml.XmlNodeType]::EndElement;
-$prolog = [System.Xml.XmlNodeType]::XmlDeclaration;
-$indent = 0;
-# $Depth = @{ Old = 0;
-#             New = 0;}
+$indentVal = 0;
 $Depth = 0;
-New-Variable -Name indentInc -Value 4 -Option ReadOnly `
+New-Variable -Name indentInc -Value 4 -Option Constant `
              -Description 'Amount by which text is indented/decremented by';
-$tagname = "";
-$padchar = "";
-$reader = [System.Xml.XmlReader]::Create($ff, $settings);
 
 Write-Output 'Start of test';
 
-while ($reader.Read()) {
+while ($xreader.Read()) {
 
-    if ($reader.Depth -gt $Depth) {
+    if ($xreader.Depth -gt $Depth) {
         # Increase the indent.
-        $indent += $indentInc;
-    } elseif ($reader.Depth -lt $Depth) {
+        $indentVal += $indentInc;
+    } elseif ($xreader.Depth -lt $Depth) {
         # Decrease the indent.
-        $indent -= $indentInc;
+        $indentVal -= $indentInc;
     }
 
     $xmlline.Length = 0;
-    switch ($reader.NodeType) {
+    switch ($xreader.NodeType) {
         $element {
             # A start element tag
-            $xmlline.Append("".PadLeft($indent, " ")) | Out-Null;
+            $xmlline.Append("".PadLeft($indentVal, " ")) | Out-Null;
             $xmlline.Append('<') | Out-Null;
-            $xmlline.Append($($reader.Name)) | Out-Null;
+            $xmlline.Append($($xreader.Name)) | Out-Null;
             $xmlline.Append('>') | Out-Null;
             Write-Output $xmlline.ToString();
 
-            Write-Verbose("Depth = {0}" -f $reader.Depth);
+            Write-Verbose("Depth = {0}" -f $xreader.Depth);
             break;}
         $endelement {
             # An end element tag
-            #$tagname = -Join ($reader.Name, ">");
+            #$tagname = -Join ($xreader.Name, ">");
             #$tagname = -Join ('</', 'parttwo', '>');
-            #$tagname = -Join ($reader.Name, '>');
+            #$tagname = -Join ($xreader.Name, '>');
 
-            #$tagname = ("{0,$($indent)}{1}>" -f "</", $reader.Name);
-            #Write-Output("{0,$($indent)}{1}" -f "</", $($tagname));
-            #Write-Output("{0,$($indent)}{1}" -f '</', $reader.Name, ">");
-            $xmlline.Append("".PadLeft($indent, " ")) | Out-Null;
+            #$tagname = ("{0,$($indentVal)}{1}>" -f "</", $xreader.Name);
+            #Write-Output("{0,$($indentVal)}{1}" -f "</", $($tagname));
+            #Write-Output("{0,$($indentVal)}{1}" -f '</', $xreader.Name, ">");
+            $xmlline.Append("".PadLeft($indentVal, " ")) | Out-Null;
             $xmlline.Append('</') | Out-Null;
-            $xmlline.Append($($reader.Name)) | Out-Null;
+            $xmlline.Append($($xreader.Name)) | Out-Null;
             $xmlline.Append('>') | Out-Null;
             Write-Output $xmlline.ToString();
-            
-            #Write-Output ("{0,$($indent)}" -f 'hellotagname');
-            Write-Verbose("Depth = {0}" -f $reader.Depth);
-            Write-Verbose("indent = {0}" -f $indent);
+
+            #Write-Output ("{0,$($indentVal)}" -f 'hellotagname');
+            Write-Verbose("Depth = {0}" -f $xreader.Depth);
+            Write-Verbose("indent = {0}" -f $indentVal);
             break;}
         default {break;}
     }# end switch
-    $Depth = $reader.Depth;
+    $Depth = $xreader.Depth;
 } #end WHILE loop
 
-$reader.Dispose();
-Remove-Variable indentInc -Force;
+$xreader.Dispose();
 Write-Output 'All done now';
 
 ##=============================================
