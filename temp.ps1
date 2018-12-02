7091e110a49a7a0dfd91ddd8e189e9b1f07f17dc
ian8667 <ianm785@outlook.com>
2018-12-02 01:21:22 +0000

    Newer version of file.

--- temp.ps1
+++ temp.ps1
@@ -1,58 +1,45 @@
-# temp.ps1
-# A temporary scratch file only.
-# Creating environment variables suitable for using the PowerShell
-# console with Oracle. With this, I can use SQL*Plus within the
-# PowerShell console.
+# Sample menu option choice.
 #
-# Last updated: 25 November 2018 16:07:58
-#
-[CmdletBinding()]
-Param () #end param
+do {
 
-##=============================================
-## SCRIPT BODY
-## Main routine starts here
-##=============================================
-Set-StrictMode -Version Latest;
-$ErrorActionPreference = "Stop";
+    do {
+        Write-Host ""
+        Write-Host "A - Selection A"
+        Write-Host "B - Selection B"
+        Write-Host "C - Selection C"
+        Write-Host "D - Selection D"
+        Write-Host ""
+        Write-Host "X - Exit"
+        Write-Host ""
+        Write-Host -NoNewline "Type your choice and press Enter: "
 
-$params =  @{
-    'ORACLE_SID'      = 'ORCL';
-    'ORACLE_HOME'     = 'C:\Family\EmailsSent\contents';
-    'NLS_DATE_FORMAT' = 'RRRR-MM-DD HH24:MI:SS';
-}
+        $choice = Read-Host
+        Write-Host "";
 
-$params99 = New-Object PSObject -Property @{
-    'ORACLE_SID'      = 'ORCL';
-    'ORACLE_HOME'     = 'C:\Test2';
-    #'TNS_ADMIN'       = Join-Path -Path $params.Item('ORACLE_HOME') -ChildPath "/NETWORK/ADMIN";
-    'NLS_DATE_FORMAT' = 'RRRR-MM-DD HH24:MI:SS';
-}
+        $ok = $choice -match '^[abcdx]+$'
 
-# Check the Oracle Home exists. If not, throw a terminating error.
-$dd = $params.ORACLE_HOME;
-#$dd = $params.Item('ORACLE_HOME');
-if (Test-Path -Path $dd) {
-    $key = 'TNS_ADMIN';
-    $val = Join-Path -Path $params['ORACLE_HOME'] -ChildPath "/NETWORK/ADMIN";
-    $params.Add($key, $val);
-} else {
-    throw [System.IO.DirectoryNotFoundException] "Oracle Home $dd not found.";
-}
+        if ( -not $ok) { Write-Error -Message "Invalid selection" }
+    } until ( $ok )
 
-$params
+    switch -Regex ( $choice ) {
+        "A"
+        {
+            Write-Host "You entered 'A'"
+        }
 
-$fred = [System.EnvironmentVariableTarget]::Process;
-Set-Variable -Name 'fred' -Option ReadOnly;
+        "B"
+        {
+            Write-Host "You entered 'B'"
+        }
 
-# Within this loop:
-# kvp.Key - the environment variable name.
-# kvp.Value - the value assigned to the variable.
-foreach ($kvp in $params.GetEnumerator()) {
-    [System.Environment]::SetEnvironmentVariable($kvp.Name, $kvp.Value, $fred);
-    Write-Verbose -Message ("Environment variable: {0},     {1}" -f $kvp.Name, $kvp.Value);
-}
+        "C"
+        {
+            Write-Host "You entered 'C'"
+        }
 
-##=============================================
-## END OF SCRIPT: temp.ps1
-##=============================================
+        "D"
+        {
+            Write-Host "You entered 'D'"
+        }
+    }
+} until ( $choice -match "X" )
