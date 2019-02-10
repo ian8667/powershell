49bd41eb47d67b58cd68da3a07db09d1a7f5997f
Ian <ian3924@yahoo.co.uk>
2019-02-10 01:49:17 +0000

    User choice from a hashtable
    Test program to store items in a hashtable. A do/until loop is then used to limit the user choice from this list of items. So when items are shown in the loop as 0 to 6, for example, the user has to enter an integer value in the range 0 to 6.

--- temp.ps1
+++ temp.ps1
@@ -3,38 +3,46 @@
 #
 $fred = New-Object -TypeName 'System.Collections.Generic.List[String](50)';
 [Int32]$response = -1;
+[Int32]$hashIndex = 0;
+$hash = @{}
 
-# Look for any MS Windows services of interest.
+
+# Look for any MS Windows services of interest adding them to
+# the hashtable as we go along. The possibility exists we won't
+# find any services of interest.
 Get-WmiObject -Class 'Win32_Service' |
   Where-Object {($_.Name -match '^R') -and ($_.State -eq "Running")} |
-  ForEach-Object {$fred.Add($_.Name)}
+  ForEach-Object {$hash.Add($hashIndex, $_.Name); $hashIndex++}
 
-if ($fred.Count -eq 0) {
+if ($hash.Count -eq 0) {
   Write-Output "No items to process. Nothing more to do";
 } else {
 
-  # Add the exit option to the list.
-  $fred.Add('Exit');
+  # Add the exit option to the hashtable.
+  $hash.Add($hashIndex, 'Exit');
 
-  # We have to wait until we've populated our List<T> object
+  # We have to wait until we've populated our hashtable object
   # first before we can create these two variables as there
   # is a dependency.
-  $Limit = @{"Min" = 0; "Max" = ($fred.count - 1)}
-  $promptMsg = [String]::Format("Please enter a number [0 - $(($fred.count) - 1 )] ");
+  $Limit = @{"Min" = 0; "Max" = ($hash.Count - 1)}
+  $promptMsg = [String]::Format("Please enter a number [0 - $(($hash.count) - 1 )] ");
 
 
   Write-Host '';
   do {
-      [Byte]$choiceCounter = 0;
-      foreach ($item in $fred.GetEnumerator()) {
-         Write-Host ("{0}    {1}" -f $choiceCounter++, $item);
+
+      # Loop through the hashtable so we have an idea what
+      # choices (options) we have to choose from.
+      foreach ($item in $hash.GetEnumerator() | Sort-Object Name) {
+         Write-Output ("{0}     {1}" -f $item.Name, $item.Value);
       }
 
+      # Obtain the required choice.
       $response = Read-Host -Prompt $promptMsg;
   } until ($response -in $Limit.Min..$Limit.Max)
 
   Write-Host 'Looking at an item';
-  $resp = $fred[$response];
+  $resp = $hash[$response];
   Write-Host ("response is now: {0} which is {1}" -f $response, $resp);
 
 }
