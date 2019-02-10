eaf7bc9ca3e41733dacda37605f61086afa1078c
Ian <ian3924@yahoo.co.uk>
2019-02-10 00:42:57 +0000

    do/until loop and user choice
    Test program to store items in the object 'System.Collections.Generic.List'. A do/until loop is then used to limit the user choice from this list of items. So when items are shown in the loop as 0 to 6, for example, the user has to enter an integer value in the range 0 to 6.

--- temp.ps1
+++ temp.ps1
@@ -1,52 +1,42 @@
-# Sample menu option choice.
+# List<T> Class
+# https://docs.microsoft.com/en-us/dotnet/api/system.collections.generic.list-1?view=netframework-4.7.2
 #
-$choiceData = DATA {
-    ""
-    "A - Selection A"
-    "B - Selection B"
-    "C - Selection C"
-    "D - Selection D"
-    ""
-    "X - Exit"
-    ""
+$fred = New-Object -TypeName 'System.Collections.Generic.List[String](50)';
+[Int32]$response = -1;
+
+# Look for any MS Windows services of interest.
+Get-WmiObject -Class 'Win32_Service' |
+  Where-Object {($_.Name -match '^R') -and ($_.State -eq "Running")} |
+  ForEach-Object {$fred.Add($_.Name)}
+
+if ($fred.Count -eq 0) {
+  Write-Output "No items to process. Nothing more to do";
+} else {
+
+  # Add the exit option to the list.
+  $fred.Add('Exit');
+
+  # We have to wait until we've populated our List<T> object
+  # first before we can create these two variables as there
+  # is a dependency.
+  $Limit = @{"Min" = 0; "Max" = ($fred.count - 1)}
+  $promptMsg = [String]::Format("Please enter a number [0 - $(($fred.count) - 1 )] ");
+
+
+  Write-Host '';
+  do {
+      [Byte]$choiceCounter = 0;
+      foreach ($item in $fred.GetEnumerator()) {
+         Write-Host ("{0}    {1}" -f $choiceCounter++, $item);
+      }
+
+      $response = Read-Host -Prompt $promptMsg;
+  } until ($response -in $Limit.Min..$Limit.Max)
+
+  Write-Host 'Looking at an item';
+  $resp = $fred[$response];
+  Write-Host ("response is now: {0} which is {1}" -f $response, $resp);
+
 }
 
-do {
-
-    do {
-
-        $num = $choiceData.Count - 1;
-        foreach ($m in 0..$num) {Write-Host ("{0}" -f $choiceData[$m])}
-
-        Write-Host -NoNewline "Type your choice and press Enter: ";
-
-        $choice = Read-Host;
-        Write-Host "";
-
-        $ok = $choice -match '^[abcdx]+$';
-
-        if ( -not $ok) { Write-Error -Message "Invalid selection" }
-    } until ( $ok )
-
-    switch -Regex ( $choice ) {
-        "A"
-        {
-            Write-Host "You entered 'A'";
-        }
-
-        "B"
-        {
-            Write-Host "You entered 'B'";
-        }
-
-        "C"
-        {
-            Write-Host "You entered 'C'";
-        }
-
-        "D"
-        {
-            Write-Host "You entered 'D'";
-        }
-    }
-} until ( $choice -match "X" )
+Write-Host 'End of test';
