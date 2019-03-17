4bc135291b9098c80192aa8035e8bedbc57eb917
Ian <ian3924@yahoo.co.uk>
2019-03-17 23:28:35 +0000

    Minor code changes
    Minor code changes to tidy up the program a little bit.

--- ianm_setenv.ps1
+++ ianm_setenv.ps1
@@ -39,7 +39,8 @@ No .NET Framework types of objects are output from this script.
 
 File Name    : ianm_setenv.ps1
 Author       : Ian Molloy
-Last updated : 2018-12-01
+Last updated : 2019-03-17
+Keywords     : oracle environment variable setenv
 
 .LINK
 
@@ -68,50 +69,71 @@ Param () #end param
 Set-StrictMode -Version Latest;
 $ErrorActionPreference = "Stop";
 
+# Allows us to count the number of environment variables
+# created.
+[Byte]$envCounter = 0;
+
 # Environment variables to create
 $params =  @{
-    'ORACLE_SID'      = 'ORCL';
-    'ORACLE_HOME'     = 'C:\Family\EmailsSent\contents';
-    'NLS_DATE_FORMAT' = 'RRRR-MM-DD HH24:MI:SS';
+  'ORACLE_SID'      = 'ORCL';
+  'ORACLE_HOME'     = 'C:\Family\EmailsSent\contents';
+  'NLS_DATE_FORMAT' = 'RRRR-MM-DD HH24:MI:SS';
 }
 
 # Check 1 - Oracle Home
-# Check the Oracle Home exists. If so, create environment variable
-# TNS_ADMIN else throw a terminating error.
-$dd = $params.ORACLE_HOME;
+# Check the Oracle home exists. If so, create environment
+# variable TNS_ADMIN which is also based upon the Oracle
+# home just validated. If not, throw a terminating error.
+$oh = $params.ORACLE_HOME;
 #$dd = $params.Item('ORACLE_HOME');
-if (Test-Path -Path $dd) {
+if (Test-Path -Path $oh) {
     $key = 'TNS_ADMIN';
-    $val = Join-Path -Path $params['ORACLE_HOME'] -ChildPath "/NETWORK/ADMIN";
+    $val = Join-Path -Path $oh -ChildPath "/NETWORK/ADMIN";
     $params.Add($key, $val);
+
     Set-Variable -Name 'params' -Option ReadOnly;
 } else {
-    throw [System.IO.DirectoryNotFoundException] "Oracle Home $dd not found.";
+    throw [System.IO.DirectoryNotFoundException] "Oracle Home $oh not found.";
 }
 
+
+# An enumerated type used to specify the environment variable
+# created will be stored in the environment block associated
+# with the current session only. In other words, these variables
+# will not persist across different PowerShell sessions and will
+# be deleted when you exit the current PowerShell session.
+$proc = [System.EnvironmentVariableTarget]::Process;
+Set-Variable -Name 'proc' -Option ReadOnly;
+
+
 # Check 2 - MS Windows Oracle service
-# For the purpose of this test, it's assumed the Oracle MS Windows
-# service name for instance 'XYZ', for example, will be in the
-# format of:
-# OracleServiceXYZ.
-$svc = "OracleService$($params.ORACLE_SID)";
+# This test is simply to warn the user if they don't have an
+# MS Windows service for the ORACLE_SID environment variable
+# just created. For the purpose of this test, it’s assumed
+# the service name concerned will be in the format of:
+#   "OracleService<ORACLE_SID>".
+$sid = [System.Environment]::GetEnvironmentVariable("ORACLE_SID", $proc);
+$svc = "OracleService$($sid)";
 if (-not (Get-Service -Name $svc -ErrorAction SilentlyContinue)) {
-    Write-Warning -Message "Can't find an MS Windows service for ORACLE_SID supplied: $($params.ORACLE_SID)";
+    Write-Output "";
+    Write-Warning -Message "Nothing to worry about, but can't find an MS Windows service for ORACLE_SID supplied: $($params.ORACLE_SID)";
     Write-Output "";
 }
 
 
-$fred = [System.EnvironmentVariableTarget]::Process;
-Set-Variable -Name 'fred' -Option ReadOnly;
-
-# Within this loop to create the environment variables:
-# kvp.Key - the environment variable name.
-# kvp.Value - the value assigned to the variable.
+# Create the environment variables. The following is true
+# Within this loop:
+#   kvp.Key - the environment variable name.
+#   kvp.Value - the value assigned to the variable.
 foreach ($kvp in $params.GetEnumerator()) {
-    [System.Environment]::SetEnvironmentVariable($kvp.Name, $kvp.Value, $fred);
+    [System.Environment]::SetEnvironmentVariable($kvp.Name, $kvp.Value, $proc);
     Write-Verbose -Message ("Environment variable: {0},     {1}" -f $kvp.Name, $kvp.Value);
+
+    $envCounter++;
 }
 
+
+Write-Output "`nOracle environment variables created: $($envCounter)";
 ##=============================================
 ## END OF SCRIPT: ianm_setenv.ps1
 ##=============================================
