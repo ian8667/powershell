7f267e60694c47a1fe5c464f0bd1f70636e46693
ian8667 <ianm785@outlook.com>
2019-05-04 23:43:42 +0100

    Added function Compress-Zip and Check-Zipfiles which validates input and output objects used.

--- Compress-File.ps1
+++ Compress-File.ps1
@@ -1,8 +1,87 @@
 <#
 #>
 
+
 [CmdletBinding()]
 Param () #end param
+
+#region ***** function Check-Zipfiles *****
+function Check-Zipfiles {
+[CmdletBinding()]
+[OutputType([System.Double])]
+Param (
+        [parameter(Mandatory=$true,
+                   Position=0)]
+        [ValidateNotNullOrEmpty()]
+        [System.String]
+        $InputDirectory,
+
+        [parameter(Mandatory=$true,
+                   Position=1)]
+        [ValidateNotNullOrEmpty()]
+        [System.String]
+        $OutputFile
+      ) #end param
+
+  if ((Get-Item $InputDirectory).PSIsContainer -ne $true)
+  {
+      throw "Input directory $($InputDirectory) has to be a directory";
+  }
+
+  if ($InputDirectory -eq $OutputFile)
+  {
+      $msg = ("Input directory {0} cannot be the same as the output file {1}" -f $InputDirectory, $OutputFile);
+      throw $msg;
+  }
+
+  $FileCount = Get-ChildItem -Path $InputDirectory;
+  if (($FileCount | Measure-Object).Count -eq 0)
+  {
+     throw "No files to compress in directory $($InputDirectory)";
+  }
+
+  if (Test-Path -Path $OutputFile)
+  {
+     $mask = 'yyyy-MM-ddTHH-mm-ss';
+     $dateTime = (Get-Date).ToString($mask);
+
+     $pos = $OutputFile.LastIndexOf([System.IO.Path]::GetExtension($OutputFile));
+     $template = $OutputFile.Insert($pos, "_{0}");
+
+     $newFilename = ($template -f $dateTime);
+     Rename-Item -Path $OutputFile -NewName $newFilename;
+  }
+
+}
+#endregion ***** end of function Check-Zipfiles *****
+
+
+#region ***** function Check-Gzipfiles *****
+function Check-Gzipfiles {
+[CmdletBinding()]
+[OutputType([System.Double])]
+Param (
+        [parameter(Mandatory=$true,
+                   Position=0)]
+        [ValidateScript({Test-Path -Path $_})]
+        [System.String]
+        $InputFile,
+
+        [parameter(Mandatory=$true,
+                   Position=1)]
+        [ValidateNotNullOrEmpty()]
+        [System.String]
+        $OutputFile
+      ) #end param
+
+  if ($InputFile -eq $OutputFile)
+  {
+      $msg = ("Input file {0} cannot be the same as the output file {1}" -f $InputFile, $OutputFile);
+      throw $msg;
+  }
+}
+#endregion ***** end of function Check-Gzipfiles *****
+
 
 #region ***** function Compress-Gzip *****
 function Compress-Gzip {
@@ -11,16 +90,19 @@ function Compress-Gzip {
 Param () #end param
 
 BEGIN {
-$bufSize = 4KB;
 
 $file = [PSCustomObject]@{
     # Input and output files used
-    Input     = 'C:\test\gashInput.txt';
-    Output    = 'C:\test\gashOutput.gz';
+    'Input'     = 'C:\test\gashInput_02.txt';
+    'Output'    = 'C:\test\gashOutput.gz';
 }
 
+Check-Gzipfiles -InputFile $file.Input -OutputFile $file.Output;
+
+$bufSize = 4KB;
+
 $optIn = [PSCustomObject]@{
-    # Input file
+    # Input file options
     path        = $file.Input;
     mode        = [System.IO.FileMode]::Open;
     access      = [System.IO.FileAccess]::Read;
@@ -30,7 +112,7 @@ $optIn = [PSCustomObject]@{
 }
 
 $optOut = [PSCustomObject]@{
-    # Output file
+    # Output file options
     path        = $file.Output;
     mode        = [System.IO.FileMode]::Create;
     access      = [System.IO.FileAccess]::Write;
@@ -39,6 +121,8 @@ $optOut = [PSCustomObject]@{
     options     = [System.IO.FileOptions]::None;
 }
 
+$CompressStart = Get-Date;
+Write-Output ('Gzip compress file start: {0}' -f $CompressStart.ToString("yyyy-MM-ddTHH-mm-ss"))
 } #end BEGIN block
 
 PROCESS {
@@ -62,7 +146,6 @@ try {
 } catch {
     Write-Error -Message $Error[0].Exception;
 } finally {
-    Write-Output 'Tidyup section';
     $gzipStream.Close();
     $gzipStream.Dispose();
     $fos.Close();
@@ -74,8 +157,16 @@ try {
 } #end PROCESS block
 
 END {
-    Write-Output "Files used`n$($file)";
-    Write-Output 'All done now';
+    $CompressFinish = Get-Date;
+    Write-Output ('Compress finish: {0}' -f $CompressFinish.ToString("yyyy-MM-ddTHH-mm-ss"))
+    $tspan = $CompressFinish - $CompressStart;
+    Write-Output "`nElapsed time:";
+    $tspan | Format-Table Days, Hours, Minutes, Seconds
+    Write-Output "Files used:";
+    Write-Output ("Input - {0}" -f $file.Input);
+    Write-Output ("Output - {0}" -f $file.Output);
+    Get-ChildItem $file.Input, $file.Output;
+    Write-Output "`nAll done now";
 }
 
 }
@@ -88,14 +179,46 @@ function Compress-Zip {
 Param () #end param
 
 BEGIN {
-write-output 'this is a pretend Compress-Zip;'
+
+$file = [PSCustomObject]@{
+    # Input and output objects used
+    'Input'     = 'C:\test\Blankdir'; # Has to be a directory
+    'Output'    = 'C:\test\gashOutput.zip';
+}
+
+Check-Zipfiles -InputDirectory $file.Input -OutputFile $file.Output;
+
+
+Add-Type -AssemblyName "System.IO.Compression.FileSystem";
+
+$opt = [System.IO.Compression.CompressionLevel]::Optimal;
+$includeBaseDirectory = $false;
+
+$CompressStart = Get-Date;
+Write-Output ('Zip compress directory start: {0}' -f $CompressStart.ToString("yyyy-MM-ddTHH-mm-ss"))
 } #end BEGIN block
 
 PROCESS {
+
+[System.IO.Compression.ZipFile]::CreateFromDirectory( `
+            $file.Input, `
+            $file.Output, `
+            $opt, `
+            $includeBaseDirectory);
+
 } #end PROCESS block
 
 END {
-    Write-Output 'All done now';
+    $CompressFinish = Get-Date;
+    Write-Output ('Compress finish: {0}' -f $CompressFinish.ToString("yyyy-MM-ddTHH-mm-ss"))
+    $tspan = $CompressFinish - $CompressStart;
+    Write-Output "`nElapsed time:";
+    $tspan | Format-Table Days, Hours, Minutes, Seconds
+    Write-Output "Directory and output file used:";
+    Write-Output ("Input - {0}" -f $file.Input);
+    Write-Output ("Output - {0}" -f $file.Output);
+    Get-ChildItem $file.Input, $file.Output;
+    Write-Output "`nAll done now";
 }
 
 }
@@ -108,23 +231,19 @@ END {
 Set-StrictMode -Version Latest;
 $ErrorActionPreference = "Stop";
 
-Write-Output 'test for Compress-File.ps1';
-
 enum CompressFormat 
 {
    Gzip;
    Zip;
 }
 
-[CompressFormat]$Choice = [CompressFormat]::Gzip;
+[CompressFormat]$Choice = [CompressFormat]::Zip;
 
 switch ($Choice)
 {
   "Gzip"   {Compress-Gzip; break;} 
   "Zip"    {Compress-Zip; break;} 
 }
-
-Write-Output 'test for Compress-File.ps1 done';
 
 ##=============================================
 ## END OF SCRIPT: Compress-File.ps1
