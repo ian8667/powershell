0465df19a5fdb82372e0dffd9015ea86ca800538
Ian <ian3924@yahoo.co.uk>
2020-05-14 13:35:53 +0100

    Implemented parameter validation.
    Code improvements and refactoring. Implemented parameter validation on the program parameters.

--- Get-StringChars.ps1
+++ Get-StringChars.ps1
@@ -12,22 +12,23 @@ characters from the input string. Any other number of characters
 will generate an error.
 
 This program came about after using security validation procedures
-which ask for randomly selected characters from a password or user
-ID. The program saves me from hunting down these characters and will
-display them for me. I.e., if I want the characters from positions one
-and seven, for example, then 1,7 should be supplied to parameter
-'CharPositions'.
+which ask for randomly selected characters from a password, phrase
+or user ID. The program saves me from hunting down these characters
+and will display them for me. I.e., if I want the characters from
+positions one and seven, for example, then 1,7 should be supplied
+to parameter 'CharPositions'.
 
 .PARAMETER Phrase
 
-(mandatory) contains the password or User ID which will be searched
-for characters to be displayed.
+(mandatory) contains the password or phrase from which characters
+will be extracted in order to be displayed to the user.
 
 .PARAMETER CharPositions
 
-(mandatory) a byte array containing the positions from which to display
-the requested characters. If the first, second and fifth characters are
-required, then enter a value of 1,2,5.
+(mandatory) a [System.Byte] array containing the positions from
+which to display (extract) the requested characters. If the
+first, second and fifth characters are required, then enter a
+value of 1,2,5.
 
 Due to the intended use of this program, only two or three characters
 can be selected. Any other number of integer digits entered will
@@ -35,15 +36,19 @@ generate an error.
 
 .EXAMPLE
 
-./Get-StringChars.ps1 -Phrase helloworld -CharPositions 2,3,5
+./Get-StringChars.ps1 -Phrase 'helloWorld' -CharPositions 2,4,9
 
-Displays characters at positions 2,3 and 5 from the phrase helloworld.
+Displays characters at positions 2,4 and 9 from the phrase helloworld.
+The positions used are one-based indices. In this example, we have the
+options of specifying positions 1 to 10 inclusive.
 
 .EXAMPLE
 
-./Get-StringChars.ps1 -Phrase helloworldtwo -CharPositions 8,12
+./Get-StringChars.ps1 -Phrase 'TheQuickBrownFox' -CharPositions 5,12
 
-Displays characters at positions 8 and 12 from the phrase helloworldtwo.
+Displays characters at positions 5 and 12 from the phrase TheQuickBrownFox.
+The positions used are one-based indices. In this example, we have the
+options of specifying positions 1 to 16 inclusive.
 
 .NOTES
 
@@ -53,7 +58,7 @@ example.
 
 File Name    : Get-StringChars.ps1
 Author       : Ian Molloy
-Last updated : 2017-12-31
+Last updated : 2020-05-14T12:52:32
 
 .LINK
 
@@ -66,11 +71,13 @@ https://technet.microsoft.com/en-us/library/hh847834.aspx
 https://ramblingcookiemonster.wordpress.com/2013/12/08/building-powershell-functions-best-practices/
 
 #>
+
 [CmdletBinding()]
 Param (
     [parameter(Position=0,
                Mandatory=$true,
-               HelpMessage="Phrase to look at")]
+               HelpMessage="Phrase or password to look at")]
+    [ValidateScript({$_.Length -ge 5})]
     [String]$Phrase,
     [parameter(Position=1,
                Mandatory=$true,
@@ -79,6 +86,84 @@ Param (
     [Byte[]]$CharPositions
 ) #end param
 
+#region ***** function Get-LetterCase *****
+function Get-LetterCase {
+[CmdletBinding()]
+Param (
+    [parameter(Position=0,
+               Mandatory=$true,
+               HelpMessage="Character to determine whether it is upper or lower case")]
+    [ValidateScript({$_.ToString().Length -eq 1})]
+    [Char]$Letter
+) #end param
+
+[String]$case = '';
+$num = [Byte]$Letter;
+switch ($num) {
+    {65..90 -contains $PSItem} {$case = "(uppercase)"; Break}
+    {97..122 -contains $PSItem} {$case = "(lowercase)"; Break }
+    default {$case = ""; Break }
+}
+
+return $case;
+}
+#endregion ***** end of function Get-LetterCase *****
+
+#region ***** function Indicate-Positions *****
+function Indicate-Positions {
+[CmdletBinding()]
+Param (
+    [parameter(Position=0,
+               Mandatory=$true,
+               HelpMessage="Phrase or password to look at")]
+    [ValidateScript({$_.Length -ge 5})]
+    [String]$Phrase,
+    [parameter(Position=1,
+               Mandatory=$true,
+               HelpMessage="Character positions of interest to look at")]
+    [ValidateCount(2,3)]
+    [Byte[]]$ByteArray
+) #end param
+
+$len = $Phrase.Length;
+$ba = New-Object -TypeName System.Collections.BitArray -ArgumentList $len;
+$sb = New-Object -TypeName System.Text.StringBuilder -ArgumentList $len, $len;
+
+# Manages a compact array of bit values, which are represented
+# as Booleans, where true indicates that the bit is on (1) and
+# false indicates the bit is off (0).
+#
+# A [System.Byte] array contains the integer positions which
+# indicate which characters to display (extract) from the
+# supplied input string. For every integer in this array, the
+# corresponding bit position in the BitArray object will be
+# set to true.
+#
+# The BitArray object is zero-based so this is why we
+# subtract one from the actual value from the [System.Byte]
+# array. If, for example, we have a value of 1 in the Byte
+# array, we subtract 1 from this in order to set position
+# 0 (zero) in the BitArray object to true which indicates
+# a position of interest in the supplied input string.
+foreach ($item in $ByteArray) {
+    Write-Verbose "Setting for pos $item";
+    $ba.Set(($item-1), $true) | Out-Null;
+}
+
+# For every zero-based index position in the BitArray object
+# set to true, set the corresponding StringBuilder object
+# position with a 'Caret - circumflex' character else set the
+# StringBuilder object position with a space.
+[String]$m = '';
+foreach ($num in 0..($len-1)) {
+    #
+    $m = (($ba.Item($num)) ? "^" : " "); #Ternary operator
+    $sb.Append($m) | Out-Null;
+}
+
+return $($sb.ToString());
+}
+#endregion ***** end of function Indicate-Positions *****
 
 #--------------------------------------------------------------------
 # Main routine starts here
@@ -86,29 +171,52 @@ Param (
 Set-StrictMode -Version Latest;
 $ErrorActionPreference = "Stop";
 
-# Convert the character positions of the input array to
-# a string so we can display them later to remind us
-# which character positions of the input phrase we're
-# looking for.
-[System.Char[]]$myArray = $CharPositions.ToString();
+# Validate the parameters supplied. Throw a terminating error
+# if any parameters are invalid.
+Invoke-Command -ScriptBlock {
+  # The minimum value in array 'CharPositions' should be one (1).
+  [Byte]$validate = ($CharPositions | Measure-Object -Minimum).Minimum;
+  if ($validate -eq 0) {
+      throw [System.ArgumentOutOfRangeException] "Minimum value allowed for parameter CharPositions is 1"
+  }
 
-# Display the phrase supplied.
-Write-Host ("`nPhrase supplied: {0}     ({1} characters)" -f `
-            [System.String]::Join("", $Phrase), $Phrase.Length);
+  # Make sure the maximum value in array 'CharPositions'
+  # is not greater than the length of string 'Phrase'.
+  $validate = ($CharPositions | Measure-Object -Maximum).Maximum;
+  if ($validate -gt $Phrase.Length) {
+      $errmsg = "For the password/phrase supplied, values in parameter CharPositions cannot be greater than $($Phrase.Length)";
+      throw [System.ArgumentOutOfRangeException] $errmsg;
+  }
 
-# Show which character positiions we're looking for.
-Write-Host ("Looking for characters at Positions: {0}" -f `
-            [System.String]::Join(", ", $CharPositions));
-Write-Host "";
+} #end scriptblock checks
+
+
+# Display the phrase or password supplied and it's length.
+Write-Output ("`nPhrase supplied: {0}     ({1} characters)" -f `
+              $Phrase, $Phrase.Length);
+
+# Show which character positiions we're looking for in the
+# input string supplied.
+Write-Output ("Looking for characters at positions: {0}" -f `
+              [System.String]::Join(", ", $CharPositions));
+Write-Output "";
+
 
 # Find and display the characters requested.
 # One is subtracted from the number requested as positions in
-# the string are 0-based, the character positions requested
-# (supplied by the user) are 1-based. So if character position 1
-# is requested, it will be found at string index 0 not 1.
+# the string are 0-based, and the character positions requested
+# (supplied by the user) are 1-based. So if character at position
+# 1 is requested, it will be found at string index 0 not 1.
 foreach ($num in $CharPositions) {
-     Write-Host ('Position {0}:  {1}' -f $num, $Phrase[$num - 1]);
+     $letter = $Phrase[$num - 1];
+     $case = Get-LetterCase -Letter $letter;
+     Write-Output ('Position {0}:  {1}     {2}' -f $num, $letter, $case);
 }
+$result = Indicate-Positions -Phrase $Phrase -ByteArray $CharPositions;
+Write-Output "";
+Write-Output "Positions indicated in phrase:";
+Write-Output $Phrase;
+Write-Output $result;
 
 ##=============================================
 ## END OF SCRIPT: Get-StringChars.ps1
