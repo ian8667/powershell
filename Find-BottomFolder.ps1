41d8c6fb2971dc85f4d11fbacd2f40a41af20a1b
Ian <ian3924@yahoo.co.uk>
2020-07-04 14:33:38 +0100

    Cosmetic changes
    Made small cosmetic changes to the code.

--- Find-BottomFolder.ps1
+++ Find-BottomFolder.ps1
@@ -64,14 +64,20 @@ No .NET Framework types of objects are output from this script.
 
 File Name    : Find-BottomFolder.ps1
 Author       : Ian Molloy
-Last updated : 2019-08-08
+Last updated : 2020-07-04T14:31:15
 
 .LINK
 
 List<T> Class
-Represents a strongly typed list of objects that can be accessed
-by index. Provides methods to search, sort, and manipulate lists.
+Represents a strongly typed list of objects that
+can be accessed by index. Provides methods to search,
+sort, and manipulate lists.
 https://docs.microsoft.com/en-us/dotnet/api/system.collections.generic.list-1?view=netframework-4.8
+
+DirectoryInfo Class
+Exposes instance methods for creating, moving, and
+enumerating through directories and subdirectories.
+https://docs.microsoft.com/en-us/dotnet/api/system.io.directoryinfo?view=netcore-3.1
 
 About Comment Based Help
 https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_comment_based_help?view=powershell-6
@@ -83,27 +89,38 @@ https://stuart-moore.com/finding-bottom-folders-file-tree-using-powershell/
 #>
 
 [CmdletBinding()]
-Param () #end param
+Param() #end param
+
+#----------------------------------------------------------
+# Start of functions
+#----------------------------------------------------------
 
 #region ***** function Get-Filecount *****
 function Get-Filecount {
 [CmdletBinding()]
 [OutputType([System.Int32])]
 Param (
-        [parameter(Position=0,
-                   Mandatory=$true,
-                   HelpMessage="Directory of interest")]
-        [ValidateNotNullOrEmpty()]
-        [String]$Directory
-      ) #end param
+    [parameter(Position=0,
+               Mandatory=$true,
+               HelpMessage="Directory of interest")]
+    [ValidateNotNullOrEmpty()]
+    [String]$Directory
+) #end param
 
-  $gash = New-Object -TypeName 'System.IO.DirectoryInfo' -ArgumentList $Directory;
-  $filecount = $gash.EnumerateFiles();
+  $dirInfo = New-Object -TypeName 'System.IO.DirectoryInfo' -ArgumentList $Directory;
+  #Returns an enumerable collection of files (if any) in the
+  #current directory. From this, we'll count how many there
+  #are
+  $filecount = $dirInfo.EnumerateFiles();
 
   return [System.Linq.Enumerable]::Count($filecount);
 
 }
 #endregion ***** end of function Get-Filecount *****
+
+#----------------------------------------------------------
+# End of functions
+#----------------------------------------------------------
 
 ##=============================================
 ## SCRIPT BODY
@@ -112,31 +129,37 @@ Param (
 Set-StrictMode -Version Latest;
 $ErrorActionPreference = "Stop";
 
-$startDir = 'C:\Test';  # <-- Change accordingly
-# Get a recursive list directories starting from our start directory.
+$startDir = 'C:\Gash\gashdir';  # <-- Change accordingly
+#Get a recursive list directories starting from our start directory.
 $dirlist = Get-ChildItem -Recurse -Directory -Path $startDir;
 Set-Variable -Name 'startDir', 'dirlist' -Option ReadOnly;
 
-$children3 = New-Object -TypeName 'System.Collections.Generic.List[String](100)';
+$children3 = New-Object -TypeName 'System.Collections.Generic.List[String]()';
+$children3.Capacity = 100;
 
-[System.Linq.Enumerable]::Repeat("", 2);
+[System.Linq.Enumerable]::Repeat("", 2); #blanklines
+#Iterate through our list of directories and see what we have
 foreach ($d in $dirlist) {
 
-    $newParent = Split-Path $d.FullName -Parent;
-    #Write-Output "t is now $($newParent)";
+    $newParent = Split-Path -Path $d.FullName -Parent;
+    Write-Verbose -Message "The new parent is now: $($newParent)";
     if ($children3.Contains($newParent)) {
-        $children3.Remove($newParent) | Out-Null;
+      Write-Verbose -Message "Removing parent $($newParent)";
+      $children3.Remove($newParent) | Out-Null;
     }
+    Write-Verbose -Message "Adding item $($d.fullname)";
     $children3.Add($d.fullname);
 
 }
 
+#Iterate through our Generic.List of directories that we've collected
+#and see how files (if any) each directory has
 foreach ($item in $children3) {
-   $fcount = Get-Filecount $item;
+   $fcount = Get-Filecount -Directory $item;
    Write-Output ('{0} ({1} files in directory)' -f $item, $fcount);
 }
 
-[System.Linq.Enumerable]::Repeat("", 2);
+[System.Linq.Enumerable]::Repeat("", 2); #blanklines
 Write-Output "Start directory used: $($startDir)";
 if ($children3.Count -eq 0) {
   Write-Output "`nNo bottom filders found from start directory";
