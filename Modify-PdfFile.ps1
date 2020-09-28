c6d4cc1f0a761c14433b1b5df73ae7db09700723
Ian <ian3924@yahoo.co.uk>
2020-09-28 23:14:25 +0100

    Added class ValidatePathExistsAttribute
    Added class ValidatePathExistsAttribute which extends class System.Management.Automation.ValidateArgumentsAttribute in order to validate variable 'libdir'. This ensures the DLL library path is valid and contains at least one DLL file.

--- Modify-PdfFile.ps1
+++ Modify-PdfFile.ps1
@@ -4,7 +4,7 @@
 
 File Name    : Modify-PdfFile.ps1
 Author       : Ian Molloy
-Last updated : 2020-09-14T11:44:15
+Last updated : 2020-09-28T18:43:04
 Keywords     : pdf itext modify
 
 iText Java API docs URL
@@ -25,10 +25,47 @@ itext.io.dll
 itext.kernel.dll
 itext.layout.dll
 
+Everything you wanted to know about PSCustomObject
+https://docs.microsoft.com/en-us/powershell/scripting/learn/deep-dives/everything-about-pscustomobject?view=powershell-7
+
 #>
 
 [CmdletBinding()]
 Param() #end param
+
+#region ***** class ValidatePathExistsAttribute *****
+class ValidatePathExistsAttribute : System.Management.Automation.ValidateArgumentsAttribute
+{
+<#
+Carries out validation of variable 'libdir'.
+Serves as the base class for Validate attributes that validate
+parameter arguments. In this case, the argument supplied is
+variable 'libdir'.
+
+ValidateArgumentsAttribute Class
+https://docs.microsoft.com/en-us/dotnet/api/system.management.automation.validateargumentsattribute?view=powershellsdk-7.0.0
+
+Introduction To PowerShell Classes
+https://overpoweredshell.com/Introduction-to-PowerShell-Classes/
+
+#>
+    Validate([Object]$Arguments, [System.Management.Automation.EngineIntrinsics]$EngineIntrinsics)
+    {
+        $myargs = $Arguments;
+        if (-not (Test-Path -Path $myargs)) {
+          throw [System.IO.DirectoryNotFoundException] "DLL library directory $myargs not found";
+        }
+
+        #Ensure we have some dll files in the DLL directory
+        $dllFilter = '*.dll';
+        $dllCount = (Get-ChildItem -File -Filter $dllFilter -Path $myargs | Measure-Object).Count;
+        if ($dllCount -eq 0) {
+          throw [System.IO.FileNotFoundException] "DLL files not found in DLL directory $myargs";
+        }
+
+    }
+}
+#endregion ***** end of class ValidatePathExistsAttribute *****
 
 ##=============================================
 ## SCRIPT BODY
@@ -51,7 +88,14 @@ Invoke-Command -ScriptBlock {
 }
 
 #[System.Reflection.AssemblyName]::GetAssemblyName($Path).FullName;
-$libdir = 'C:\Ian\PowerShell\lib'; # <-- Change accordingly
+#PowerShell class 'ValidatePathExistsAttribute' checks the
+#following and throws a terminating error if these checks
+#do not succeed.
+#1. The directory path exists
+#2. The directory contains at least one dll file
+[ValidatePathExists()]
+$libdir = 'C:\Family\powershell\lib'; # <-- Change accordingly
+
 $dllfiles = @(
   "$libdir\itext.layout.dll";
   "$libdir\itext.kernel.dll";
@@ -67,27 +111,31 @@ foreach ($dll in $dllfiles) {
   Add-Type -Path $dll;
 }
 
-$src = "C:\gash\SamplePdffile.pdf"; # Existing PDF file  <-- Change accordingly
-$dest = "C:\gash\SamplePdffile_0025.pdf"; # New PDF file <-- Change accordingly
-Set-Variable -Name 'src', 'dest' -Option ReadOnly;
-if (Test-Path -Path $dest) {
-  Remove-Item -Path $dest -Force;
+$DataFile = [PSCustomObject]@{
+  # Input and output files used - Change accordingly
+  PSTypeName   = 'DataFiles';
+  Source       = 'C:\Gash\mygash.pdf'; #Existing PDF file
+  Destination  = 'C:\Gash\mygash_0025.pdf'; #New PDF file
+}
+Set-Variable -Name 'DataFile' -Option ReadOnly;
+if (Test-Path -Path $DataFile.Destination) {
+  Remove-Item -Path $DataFile.Destination -Force;
 }
 
-$reader = New-Object -typeName iText.Kernel.Pdf.PdfReader -ArgumentList $src;
+$reader = New-Object -typeName 'iText.Kernel.Pdf.PdfReader' -ArgumentList $DataFile.Source;
 $reader.SetUnethicalReading($true) | Out-Null;
 $reader.SetCloseStream($true);
-$writer = New-Object -typeName iText.Kernel.Pdf.PdfWriter -ArgumentList $dest;
+$writer = New-Object -typeName 'iText.Kernel.Pdf.PdfWriter' -ArgumentList $DataFile.Destination;
 $writer.SetCloseStream($true);
-$pdfDoc = New-Object -typeName iText.Kernel.Pdf.PdfDocument -ArgumentList $reader, $writer;
+$pdfDoc = New-Object -typeName 'iText.Kernel.Pdf.PdfDocument' -ArgumentList $reader, $writer;
 $numPages = $pdfDoc.GetNumberOfPages();
 
 $metadata = $pdfDoc.GetDocumentInfo();
-$metadata.setAuthor("Ian Molloy") | Out-Null;
-$metadata.setCreator("A simple PDF tutorial example") | Out-Null; #Application:
-$metadata.setKeywords("keyword1 keyword2") | Out-Null;
-$metadata.setSubject("PDF file testing") | Out-Null;
 $metadata.setTitle("The Strange Case of Dr. Jekyll and Mr. Hyde") | Out-Null;
+$metadata.setAuthor("Ian Molloy") | Out-Null;
+$metadata.setSubject("PDF file testing") | Out-Null;
+$metadata.setKeywords("keyword1 keyword2") | Out-Null;
+$metadata.setCreator("PowerShell script Modify-PdfFile.ps1") | Out-Null; #Field Application:
 
 #Custom keywords
 #You can add custom properties to a PDF document. Each
@@ -102,8 +150,8 @@ $pdfDoc.close();
 $writer.close();
 $reader.close();
 
-Get-ChildItem -Path $dest;
-Write-Output ('Pages in file: {0}' -f $numPages);
+Get-ChildItem -Path $DataFile.Source, $DataFile.Destination;
+Write-Output ('Pages in input file: {0}' -f $numPages);
 
 [System.Linq.Enumerable]::Repeat("", 2); #blanklines
 Write-Output 'All done now';
