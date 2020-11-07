bb90950ce44b645d994868e6f8e08a1853daeb2b
ian8667 <ianm785@outlook.com>
2020-11-07 19:09:45 +0000

    Changed some variable names to make them more meaningful.

--- Kopy-2Orig.ps1
+++ Kopy-2Orig.ps1
@@ -12,7 +12,7 @@ so this script checks first to see whether this is the
 case. If so, the file is not copied from the local Git
 respository.
 
-The directory paths used are hard coded withing the program
+The directory paths used are hard coded within the program
 
 .EXAMPLE
 
@@ -25,15 +25,15 @@ will be prompted to supply the filename.
 
 PS> ./Kopy-2Orig.ps1 'myfile.ps1'
 
-The filename supplied will be copied to the original
-PowerShell directory.
+The filename supplied will be copied to the main
+(master) PowerShell directory.
 
 .EXAMPLE
 
 PS> ./Kopy-2Orig.ps1 -Filename 'myfile.ps1'
 
-The filename supplied will be copied to the original
-PowerShell directory.
+The filename supplied will be copied to the main
+(master) PowerShell directory.
 
 .INPUTS
 
@@ -47,7 +47,7 @@ No .NET Framework types of objects are output from this script.
 
 File Name    : Kopy-2Orig.ps1
 Author       : Ian Molloy
-Last updated : 2020-08-05T23:15:06
+Last updated : 2020-11-07T18:40:06
 Keywords     : git github repository copy
 
 #>
@@ -71,7 +71,7 @@ $ErrorActionPreference = "Stop";
 Invoke-Command -ScriptBlock {
 
    Write-Output '';
-   Write-Output 'Copy file from gitrepos to original PowerShell directory';
+   Write-Output 'Copy file from local Git repository to original PowerShell directory';
    $dateMask = Get-Date -Format 'dddd, dd MMMM yyyy HH:mm:ss';
    Write-Output ('Today is {0}' -f $dateMask);
 
@@ -82,21 +82,27 @@ Invoke-Command -ScriptBlock {
 }
 
 [System.Linq.Enumerable]::Repeat("", 2); #blanklines
-#Directories
-$sourceDir = 'C:\IanmTools\GitRepos\powershell';
-$destDir = 'C:\Family\powershell';
+#Directories involved
+$ConfigData = [PSCustomObject]@{
+   #Local Git repository
+   SourceDirectory = 'C:\IanmTools\GitRepos\powershell';
+
+   #Main (master) PowerShell directory
+   DestinationDirectory = 'C:\Family\powershell';
+}
+Set-Variable -Name 'ConfigData' -Option ReadOnly;
 
 #Filenames with absolute paths
-$kopyFile = Join-Path -Path $sourceDir -ChildPath $Filename;
-$destFile = Join-Path -Path $destDir -ChildPath $Filename;
-Set-Variable -Name 'sourceDir', 'destDir', 'kopyFile', 'destFile' -Option ReadOnly;
+$SourceFile = Join-Path -Path $ConfigData.SourceDirectory -ChildPath $Filename;
+$DestinationFile = Join-Path -Path $ConfigData.DestinationDirectory -ChildPath $Filename;
+Set-Variable -Name 'SourceFile', 'DestinationFile' -Option ReadOnly;
 
 #Not all of the programs in the local Git repository will be
 #in the original PowerShell directory. If this is the case,
 #there is nothing to do.
-if (-not (Test-Path -Path $destFile)) {
+if (-not (Test-Path -Path $DestinationFile)) {
 $msg = @"
-File $Filename does not exist in the original PowerShell directory.
+File $Filename does not exist in the original (master) PowerShell directory.
 So this file will not be copied
 "@
 
@@ -104,16 +110,16 @@ So this file will not be copied
   return;
 }
 
-Write-Output ('Copying file {0} to {1}' -f $Filename, $destFile);
+Write-Output ('Copying file {0} to {1}' -f $Filename, $DestinationFile);
 Start-Sleep -Seconds 2.0;
-Copy-Item -Path $kopyFile -Destination $destDir;
+Copy-Item -Path $SourceFile -Destination $$ConfigData.DestinationDirectory;
 
 #Ensure the file copy was OK. Algorithm MD5 is being used for simple
 #change validation only. We can also use hash values to determine if
 #two different files have exactly the same content. If the hash values
 #of the files are identical, the contents of the files are also
 #identical and thus the copy of the file was OK.
-$fileHash = Get-FileHash -Path $kopyFile, $destFile -Algorithm 'MD5';
+$fileHash = Get-FileHash -Path $SourceFile, $DestinationFile -Algorithm 'MD5';
 [System.Linq.Enumerable]::Repeat("", 2); #blanklines
 if ($fileHash.Hash[0] -eq $fileHash.Hash[1]) {
    Write-Output ('File {0} seems to have copied OK ' -f $Filename);
